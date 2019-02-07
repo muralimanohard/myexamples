9b0b80ce176eb496c6da03868537183d7ba7f363
mural <mural@DESKTOP-L5VRP07>
2019-02-07 22:31:52 +0100

    added jenkins files

--- counter-microservices-series-docker/configservice/src/main/java/com/marcuseisele/springboot_microservices/configservice/ConfigserviceApplication.java
+++ counter-microservices-series-docker/configservice/src/main/java/com/marcuseisele/springboot_microservices/configservice/ConfigserviceApplication.java
@@ -10,8 +10,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableDiscoveryClient
 public class ConfigserviceApplication {
 
-	 String test1 = "delete";
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserviceApplication.class, args);
 	}
