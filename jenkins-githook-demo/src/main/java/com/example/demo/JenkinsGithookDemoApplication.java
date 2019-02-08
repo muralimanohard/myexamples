0c0bee54985433249a7a6e6855d9ea46d78d15d2
mural <mural@DESKTOP-L5VRP07>
2019-02-08 16:50:23 +0100

    jenkins-githook-demo

---
+++ jenkins-githook-demo/src/main/java/com/example/demo/JenkinsGithookDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGithookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGithookDemoApplication.class, args);
+	}
+
+}
+
